167a16a8d9ace66651833545f82500c98ba20249
DanGut19 <111399542+DanGut19@users.noreply.github.com>
2024-03-11 21:32:06 -0600

    Agregar archivos de proyecto.

---
+++ SumaAppMvvm/Models/Suma.cs
@@ -0,0 +1,13 @@
+﻿namespace SumaAppMvvm.ViewModels
+{
+    internal class Suma
+    {
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+
+        public double Calcular() 
+        {
+            return Numero1 + Numero2;
+        }
+    }
+}
---
+++ SumaAppMvvm/ViewModels/MainPageViewModels.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SumaAppMvvm.ViewModels
+{
+    internal partial class MainPageViewModels : ObservableObject
+    {
+        [ObservableProperty]
+        private double _numero1;
+
+        [ObservableProperty]
+        private double _numero2;
+
+        [ObservableProperty]
+        private double _resultado;
+
+        [RelayCommand]
+        private void Calcular() 
+        {
+            try
+            {
+                Suma r = new Suma();
+                r.Numero1 = Numero1;
+                r.Numero2 = Numero2;
+
+                Resultado = r.Calcular();
+            }
+            catch (Exception ex) {
+                MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert("ERROR", ex.Message, "Aceptar"));
+                ////DisplayAlert("Error", ex.Message, "Aceptar");
+            }
+        }
+
+        [RelayCommand]
+        private void Limpiar()
+        {
+            Numero1 = 0;
+            Numero2 = 0;
+            Resultado = 0;
+        }
+
+        [ObservableProperty]
+        private int count;
+
+        [RelayCommand]
+        public void Incrementar() 
+        {
+            count++;
+        }
+    }
+}
